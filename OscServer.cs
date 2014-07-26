08c2fd8a678bda596cb32b10feb1e95b59ad224f
Tatsuki Adaniya <t.adaniya0624@gmail.com>
2014-07-26 18:14:22 +0900

    connect oF

--- OscServer.cs
+++ OscServer.cs
@@ -9,6 +9,7 @@ public class OscServer : MonoBehaviour
     UdpClient udpClient;
     IPEndPoint endPoint;
     Osc.Parser osc = new Osc.Parser ();
+    //float msg.data = 0;
     
     void Start ()
     {
@@ -25,15 +26,20 @@ public class OscServer : MonoBehaviour
         while (osc.MessageCount > 0) {
             var msg = osc.PopMessage ();
             // /1/push5:1を_1_push5:1に置換する
+
+            Vector3 p = new Vector3((float)msg.data[0], (float)msg.data[1], (float)msg.data[2]);
+            GameObject target = GameObject.Find("OVRPlayerController");
+            target.transform.position = p;
             
-            var target = GameObject.Find("OVRPlayerController");
-            if (target) {
-            	Vector3 (float).temp;
+            /*var target = GameObject.Find("OVRPlayerController");
+            if (target){
+            	Vector3 temp =  new Vector3(0,0,0);
+            	float msg.data = 0;
   				temp.x = msg.data[1];
   				temp.z = msg.data[2];
   				temp.y = msg.data[3];
  				target.transform.position = temp;
-            }
+            }*/
             //オブジェクトにmsg.data[0]の引数を尾耐える
 
             //msgにはOSCから送られて来るデータが /1/push5:0:
